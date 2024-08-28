a2e3445366d104dae9413d197917f3bad8277528
Joshua-Production <joshuaespinosabusiness@outlook.com>
2024-08-28 10:38:02 -0500

    Initial project upload

---
+++ HelloDungeon/Game.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloDungeon
+{
+    internal class Game
+    {
+        public async void Run()
+        {
+            string playerName = "Boblius";
+            float playerHealth = 10.0f;
+            float playerMana = 5.0f;
+            int playerGold = 3;
+            float playerStamina = 100;
+            int playerXp = 0;
+            int playerHealthPotion = 5;
+            int playerManaPotion = 5;
+            int playerFood = 3;
+            string playerItem1 = "Sword";
+            string itemHp = "Durability"; 
+  
+           
+            
+            Console.WriteLine("Hello , " + playerName);
+            Console.WriteLine();
+            Console.WriteLine("Welcome to my dungeon!");
+            Console.WriteLine();
+            Console.WriteLine("Health: " + playerHealth);
+            Console.WriteLine("Mana; " + playerMana);
+            Console.WriteLine("Stamina; " + playerStamina);
+            Console.WriteLine("Xp; " + playerXp);
+            Console.WriteLine("Gold; " + playerGold);
+            Console.WriteLine("HPpot; " + playerHealthPotion);
+            Console.WriteLine("MPpot; " + playerManaPotion);
+            Console.WriteLine("Food; " +  playerFood);
+            Console.WriteLine("Item;" + playerItem1);
+            Console.WriteLine("You find yourself in await Generic dungeon " +
+                "with two doors. Which door do you chose?");
+            
+        }
+    }
+}
---
+++ HelloDungeon/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HelloDungeon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Run();
+            
+        }
+    }
+}
