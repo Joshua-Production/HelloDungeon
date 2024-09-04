0dbbf809d9db37022a97ee0427d118aca3f9ff99
Joshua-Production <joshuaespinosabusiness@outlook.com>
2024-09-04 16:29:17 -0500

    Changed how stats were stored and displayed in game.cs
    Added player1 Struct and instance into game class.
    - added getinput() function
    - added Role option and door options

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,50 +11,66 @@ namespace HelloDungeon
 {
     internal class Game
     {
-        public async void Run()
+        struct Player
         {
-            string playerName = "Boblius";
-            float playerHealth = 10.0f;
-            float playerMana = 5.0f;
-            int playerGold = 3;
-            float playerStamina = 100;
-            int playerXp = 0;
-            int playerHealthPotion = 5;
-            int playerManaPotion = 5;
-            int playerFood = 3;
-            string dooroption1 = "Door1";
-            string dooroption2 = "Door2";
-            string playerRole1 = "Warrior";
-            string playerRole2 = "Wizard";
+            public string Name;
+            public float Health;
+            public float Mana;
+            public int Stamina;
+            public int Gold;
+            public float Xp;
+            public int HealthPotion;
+            public int ManaPotion;
+            public int Food;
+            public string Role;
+            public string PlayerInput;
+            // <summary>
+            // players constructor 
+            /// </summary>
+            /// <param name="Name"></param>
+            /// <param name="Health"></param>
+            /// <param name="Mana"></param>
+            /// <param name="Stamina"></param>
+            /// <param name="Gold"></param>
+            /// <param name="Xp"></param>
+            /// <param name="HealthPotion"></param>
+            /// <param name="ManaPotion"></param>
+            /// <param name="Food"></param>
+            /// <param name="PlayerRole"></param>
+            /// <param name="PlayerInput"></param>
+            public Player(
+                string Name,
+                float Health,
+                float Mana,
+                int Stamina,
+                int Gold,
+                float Xp,
+                int HealthPotion,
+                int ManaPotion,
+                int Food,
+                string Role,
+                string PlayerInput)
 
-            Console.WriteLine("Hello , " + playerName);
-            Console.WriteLine();
-            Console.WriteLine("Welcome to my dungeon!");
-            Console.WriteLine();
-            Console.WriteLine("Health: " + playerHealth);
-            Console.WriteLine("Mana; " + playerMana);
-            Console.WriteLine("Stamina; " + playerStamina);
-            Console.WriteLine("Xp; " + playerXp);
-            Console.WriteLine("Gold; " + playerGold);
-            Console.WriteLine("HPpot; " + playerHealthPotion);
-            Console.WriteLine("MPpot; " + playerManaPotion);
-            Console.WriteLine("Food; " + playerFood);
-            int input = GetInput("Are you a warrior or a wizard?", "Warrior", "Wizard");
-
-
-            
-
-
-
-
-
-
-
+            {  
+                this.Name = Name;
+                this.Health = Health;
+                this.Mana = Mana;
+                this.Stamina = Stamina;
+                this.Gold = Gold;
+                this.Xp = Xp;
+                this.HealthPotion = HealthPotion;
+                this.ManaPotion = ManaPotion;
+                this.Food = Food;
+                this.Role = Role;
+                this.PlayerInput = PlayerInput;
+            }
         }
+
 
 
         int GetInput(string description, string option1, string option2)
         {
+            // Players Typed input
             string input = "";
             int inputRecieved = 0;
 
@@ -64,15 +82,12 @@ namespace HelloDungeon
                 Console.WriteLine("1. " + option1);
                 Console.WriteLine("2. " + option2);
                 Console.Write("> ");
-
-                // If player selected the first option
-
+                // Get input from player
                 input = Console.ReadLine();
 
                 // If player selected the first option
                 if (input == "1" || input == option1)
                 {
-
                     // Set inputRecieved to be the first option
                     inputRecieved = 1;
                 }
@@ -88,25 +103,88 @@ namespace HelloDungeon
                     // Display error message
                     Console.WriteLine("Invalid Input");
                     Console.ReadKey();
-
-
                 }
-
-
-
             }
+
             Console.Clear();
             return inputRecieved;
-
-
         }
+    
+         
+
+        public void Run()
+        {  // type name = value
+            Player player1 = new Player(" Jim", Health: 20f, Mana: 50f, Stamina: 100, Gold: 5, Xp: 0f, HealthPotion: 5, ManaPotion: 5, Food: 3, Role: "", PlayerInput: "");
+
+            Console.WriteLine(" You find yourself inside of what seems to be a cave with no exit or entrance ");
+            Console.WriteLine("");
+            Console.WriteLine(" Hello Jim"); 
+            Console.WriteLine(" I see that you found my dungeon... Now i will give you a choice would you like to be a Worrior or a Wizard? ");
+            Console.WriteLine(" Worrior = 1 | Wizzard = 2 ");
+            int input = 0;
+
+           
+            //Player Choices
+
+            input = GetInput(" Worrior = 1 | Wizzard = 2 ", " Worrior ", " Wizzard ");
+            if (input == 1)
+            {
+                player1.Role = "Warrior";
+            }
+            else if (input == 2)
+            {
+                player1.Role = "Wizard";
+            }
+
+            Console.WriteLine(" Player role: " + player1.Role);
+            Console.WriteLine("");
+
+            Console.WriteLine(" So, you chose " + player1.Role);
+            Console.WriteLine("");
+            Console.WriteLine(" These are your stats.");
+            Console.WriteLine("");
+         
+            
 
 
 
 
 
+            
+            
+
+            PrintPlayerStats(player1);
+            Console.WriteLine(" Press Any Key To Continue ");
+            Console.WriteLine("");
+            Console.ReadKey();
+
+            Console.Clear();
+
+            input = 0;
+
+            GetInput(" Two Doors appear infront of you These doors may look the same but the path they bring you on is for you to decide ", "Door 1", "Door 2");
+           
+            
 
 
+
+        }  
+
+        static void PrintPlayerStats(Player player)
+        {
+            Console.WriteLine(player.Name);  
+            Console.WriteLine( " Health:" +          player.Health);
+            Console.WriteLine( " Mana:  " +          player.Mana);
+            Console.WriteLine( " Gold:  " +          player.Gold);
+            Console.WriteLine( " Xp:    " +          player.Xp);
+            Console.WriteLine( " HealthPotions:  " + player.HealthPotion);
+            Console.WriteLine( " ManaPotion:  " +    player.ManaPotion);
+            Console.WriteLine( " Food:  " +          player.Food);
+            
+        }
+         
+
+  
     }
 }
         
